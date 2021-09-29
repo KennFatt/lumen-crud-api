fdb15772a9aebe2a674913812457c60fe01bd3e0
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-09-29 07:24:21 +0700

    Feat: Implement controller functionality
    - The CRUD actions.

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -6,6 +6,7 @@ use App\Models\Author;
 use App\Traits\ApiResponder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
@@ -20,6 +21,11 @@ class AuthorController extends Controller
     {
     }
 
+    /**
+     * Retrieve all authors record.
+     *
+     * @access GET /authors
+     */
     public function index(): ?JsonResponse
     {
         $authors = Author::all()->toArray();
@@ -27,23 +33,85 @@ class AuthorController extends Controller
         return $this->responseSucceed($authors);
     }
 
+    /**
+     * Retrieve one author record.
+     *
+     * @access GET /authors/{authorId}
+     */
     public function show(int $authorId): ?JsonResponse
     {
-        return null;
+        $author = Author::findOrFail($authorId);
+
+        return $this->responseSucceed([$author]);
     }
 
+    /**
+     * Create new author record.
+     * The request header field `Content-Type: multipart/form-data`
+     *
+     * For more information about `form-data` vs. `x-www-form-urlencoded` please read this
+     *  answer: https://stackoverflow.com/a/4073451/6569706
+     *
+     * @access POST /authors
+     */
     public function store(Request $req): ?JsonResponse
     {
-        return null;
+        $rules = [
+            "name" => "required|max:255",
+            "gender" => "required|max:255|in:male,female",
+            "country" => "required|max:255"
+        ];
+
+        $this->validate($req, $rules);
+
+        $author = Author::create($req->all());
+
+        return $this->responseSucceed([$author], Response::HTTP_CREATED);
     }
 
+    /**
+     * Update existing record with the new one.
+     * The request header field `Content-Type: application/x-www-form-urlencoded`
+     *
+     * For more information about `form-data` vs. `x-www-form-urlencoded` please read this
+     *  answer: https://stackoverflow.com/a/4073451/6569706
+     *
+     * @access PATCH /authors/{authorId}
+     * @access PUT /authors/{authorId}
+     */
     public function update(Request $req, int $authorId): ?JsonResponse
     {
-        return null;
+        $rules = [
+            "name" => "max:255",
+            "gender" => "max:255|in:male,female",
+            "country" => "max:255"
+        ];
+
+        $this->validate($req, $rules);
+
+        $author = Author::findOrFail($authorId);
+        $author->fill($req->all());
+        if ($author->isClean()) {
+            return $this->responseError(
+                "Nothing changes. Please try again.",
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+        $author->save();
+
+        return $this->responseSucceed([$author]);
     }
 
+    /**
+     * Delete an author record.
+     *
+     * @access DELETE /authors/{authorId}
+     */
     public function destroy(int $authorId): ?JsonResponse
     {
-        return null;
+        $author = Author::findOrFail($authorId);
+        $author->delete();
+
+        return $this->responseSucceed([$author]);
     }
 }
